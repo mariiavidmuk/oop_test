0c2348b6973c7e4e6e28685f8d946a6a8e92513c
mariiavidmuk <72019772+mariiavidmuk@users.noreply.github.com>
2020-11-09 13:20:39 +0200

    The third task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int amount;
+    std::cin >> amount;
+    int sum =0;
+    for(int i = 0; i < amount; i++){
+       int  a =  rand() % 10;
+       std::cout <<a<< " ";
+       sum += a;
+    }
+    std::cout << std::endl;
+    std::cout << "The sum: ";
+    std:: cout<< sum;
     return 0;
 }
